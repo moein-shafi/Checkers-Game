a9904c1854d1049610761ca5a19a906195a2794e
Moein Shafi <mosafer.moein@gmail.com>
2021-11-07 18:28:53 +0330

    Initial commit.

---
+++ minimax/minimax.py
@@ -0,0 +1,17 @@
+from copy import deepcopy
+import pygame
+
+RED = (255,0,0)
+WHITE = (255,255,255)
+
+def minimax(position, depth, maxPlayer, game):
+    pass
+    # Your Code Goes Here
+
+def simulateMove(piece, move, board, game, skip):
+    pass
+    # Your Code Goes Here
+
+def getAllMoves(board, color, game):
+    pass
+    # Your Code Goes Here
